4c06291ff015ec54a9bf959ae4e241aa4bd0a858
DEVENWAGH <wagh1.2.3.002@gmail.com>
2025-07-30 04:31:53 +0530

    Refactor environment variable assignments and clean up Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  compress: true,
-  poweredByHeader: false,
-  generateEtags: false,
-  experimental: {
-    optimizeCss: true,
-  },
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    formats: ['image/webp', 'image/avif'],
-    minimumCacheTTL: 31536000,
     remotePatterns: [
       {
         protocol: 'https',
